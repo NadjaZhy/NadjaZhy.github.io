573582ab855e89cf02df1fdd9618ceaa4fefb14b
NadjaZhy <zhvo.zhvo@yandex.ru>
2017-05-16 15:57:39 +0300

    Update script.js

--- JS_Essentials1/script.js
+++ JS_Essentials1/script.js
@@ -3,4 +3,4 @@ name = 'Вася';
 event = prompt('Enter event');
 time = prompt('Enter time of event');
 place = prompt('Enter place of event');
-console.log( name + ' has a '+ event +'  today at ' + time + ' somewhere in '+ place);
+console.log( name + ' has a '+ event +'  today at ' + time + ' somewhere in '+  place);
