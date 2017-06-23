ba071789505a9721ae171fd21d5efa0706b07cd7
NadjaZhy <nadjazhy@gmail.com>
2017-06-23 18:49:48 +0300

    Update script.js

--- JS_3/script.js
+++ JS_3/script.js
@@ -1,22 +1,86 @@
-while (true) 
-	 var i = prompt("Введите число больше 100", "");
-	    
-	  if (i == null)
-	  {
-	    alert("Вы нажали cancel");
-	    break;  
-	  }
+//Task1 Find and display total sum of numbers from 1 to 3;
+var arr = [1,2,3];
+function arraySum(array){
+var sum = 0;
+for(var i = 0; i < array.length; i++){
+    sum += array[i];
+    }
+alert(sum);
+}
+arraySum(arr);
+ 
+//Task2 Find and display total sum of numbers from 20 to 220;
+  var sum = 0;
+function sumTo(n) {
+ for (var i = 20; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+alert( sumTo(220) );
 	
-	  i = parseInt(i); 
-	 else if (isNaN(i))
-	  {    
-	     continue; 
-	  }
-	  
-	 else if (i > 100)
-	  {
-	    alert("Вы ввели нужное число");
-	    break;
-	  }
-	}
-	
+//Task3.1   Display even numbers from 1 to 20 by using for loop.do it with using ‘continue’
+for (var i = 1; i < 20; i++) {
+ 
+  if (i % 2 == 0) continue;
+ 
+document.write(i + '<br>');
+}
+ 
+//Task3.2 Display even numbers from 1 to 20 by using for loop. do it without using ‘continue’
+var i = 1;
+while (i < 20) {
+    if (i % 2) {
+        document.write(i + '<br>');
+    }
+    i++;
+}
+//Task4 Display odd numbers from 10 to 30 by using ‘while’ loop.
+var i = 20;
+while (i < 30) {
+    if (i % 2 === 0) {
+        document.write(i + '<br>');
+    }
+    i++;
+}
+//Task5 Display numbers from 1 to 15 using ‘for’ loop, once num of iteration is equal the num of month of your birthday then stop execution of loop
+for(i=1; i<15; i++) {
+ if (i==6) break;
+}
+alert(i) 
+ 
+//Task6 Rewrite code from using ‘for’ to using ‘while’
+var i = 0;
+while (i <5) {
+document.write(i + '<br>');
+i++;}
+ 
+//Task7.1 Write code which will ask user (prompt ) to put number greater than 100, repeat prompt until user put required number, then display result of prompt  do it using ‘while’
+   	var i = 0;
+   	while (i <= 100 ){
+         	i = parseInt(prompt('Enter more than 100', i));
+   	}
+   	alert('You entered ' + i);
+ 
+ 
+//Task7.2 Write code which will ask user (prompt ) to put number greater than 100, repeat prompt until user put required number, then display result of prompt  do it using ‘do while’
+var i;
+ 
+do {
+  i = prompt("Введите число больше 100?", 0);
+} while (i <= 100 && i != null);
+ 
+ 
+ 
+//Task8 Show in console using for.
+ //#
+ //##
+ //###
+ //####
+ 
+var str = '';
+var hash = "#";
+for(var i = 0; i < 8; i++){
+    str += hash;
+    console.log(str);
+}
