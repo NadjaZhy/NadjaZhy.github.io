e4fd77ce52f3a19b9914d1d50a0f80c63cac5f63
NadjaZhy <nadjazhy@gmail.com>
2017-06-23 19:03:03 +0300

    Update script.js

--- JS_3/script.js
+++ JS_3/script.js
@@ -5,7 +5,7 @@ var sum = 0;
 for(var i = 0; i < array.length; i++){
     sum += array[i];
     }
-alert(sum);
+alert('sumof 1,2,3= '+sum);
 }
 arraySum(arr);
  
@@ -17,7 +17,7 @@ function sumTo(n) {
   }
   return sum;
 }
-alert( sumTo(220) );
+alert('sum of numbers from 20 to 220= ' sumTo(220) );
 	
 //Task3.1   Display even numbers from 1 to 20 by using for loop.do it with using ‘continue’
 for (var i = 1; i < 20; i++) {
