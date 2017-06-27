1c0ff8444347f97b908d98032574d0fe16050d0f
NadjaZhy <nadjazhy@gmail.com>
2017-06-27 17:48:37 +0300

    Update script.js

--- JS_3/script.js
+++ JS_3/script.js
@@ -1,12 +1,10 @@
-//Task1 Find and display total sum of numbers from 1 to 3;
 var arr = [1,2,3];
 function arraySum(array){
 var sum = 0;
 for(var i = 0; i < array.length; i++){
     sum += array[i];
     }
-document.write('sumof 1,2,3 = '+sum+ '<br>');
+alert('sumof 1,2,3= '+sum);
 }
 arraySum(arr);
- document.write('<br>')
-
+ 
